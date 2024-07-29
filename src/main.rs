8d7e92d6861626b279ff2740360eb91fa1bf49bf
FunneMonkee <luismiguel11072000@gmail.com>
2024-07-29 22:20:00 +0100

    basic websocket impl to return the same message received

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,29 @@
-use actix_web::{middleware::Logger, App, HttpServer};
+use actix::{Actor, StreamHandler};
+use actix_web::{middleware::Logger, web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use actix_web_actors::ws::{self, Message};
+
+struct HttpActor;
+
+impl Actor for HttpActor {
+    type Context = ws::WebsocketContext<Self>;
+}
+
+impl StreamHandler<Result<Message, ws::ProtocolError>> for HttpActor {
+    fn handle(&mut self, item: Result<Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match item {
+            Ok(Message::Ping(msg)) => ctx.pong(&msg),
+            Ok(ws::Message::Text(text)) => ctx.text(text),
+            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            _ => (),
+        }
+    }
+}
+
+async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    let resp = ws::start(HttpActor {}, &req, stream);
+    print!("{:?}", resp);
+    resp
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -6,6 +31,7 @@ async fn main() -> std::io::Result<()> {
         let logger = Logger::default();
         App::new()
             .wrap(logger)
+            .route("/ws/", web::get().to(index))
     })
     .bind(("127.0.0.1", 8080))
     .expect("Failed binding to localhost")
